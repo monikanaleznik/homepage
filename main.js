dbde6a9fe94133d7d1e64ae02cc363ab114773ca
Monika Naleźnik <monikan793gmail.com>
2020-02-26 16:29:15 +0100

    lets-start-js

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ console.log(name);
 console.log(age);
 
 console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+console.log('witaj przybyszu');
+
+const about__paragraph = document.querySelector('.about__paragraph--js');
+
+console.log(about__paragraph.innerHTML);
+
+about__paragraph.innerHTML = '<strong>Pozdro</strong> JS!';
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs);
+console.log(paragraphs[0]);
+
+
